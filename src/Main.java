136721e3b7240e717d371f00cb75cd9154681d09
Tune <mikael.thune@gmail.com>
2017-01-29 21:15:14 +0100

    Button test to separate method and added circle draw test

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,67 @@
  */
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.effect.BoxBlur;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
 import javafx.stage.Stage;
 
 public class Main extends Application
 {
     @Override
     public void start(Stage primaryStage) throws Exception
+    {
+//        simpleButton(primaryStage);
+        drawSomething(primaryStage);
+        primaryStage.show();
+    }
+
+    private void drawSomething(Stage primaryStage)
+    {
+        Group root = new Group();
+        Scene scene = new Scene(root, 800, 600, Color.BLACK);
+        primaryStage.setScene(scene);
+
+        Rectangle colors = new Rectangle(scene.getWidth(), scene.getHeight(),
+                new LinearGradient(0f, 1f, 1f, 0f, true, CycleMethod.NO_CYCLE, new
+                        Stop[]{
+                        new Stop(0, Color.web("#f8bd55")),
+                        new Stop(0.14, Color.web("#c0fe56")),
+                        new Stop(0.28, Color.web("#5dfbc1")),
+                        new Stop(0.43, Color.web("#64c2f8")),
+                        new Stop(0.57, Color.web("#be4af7")),
+                        new Stop(0.71, Color.web("#ed5fc2")),
+                        new Stop(0.85, Color.web("#ef504c")),
+                        new Stop(1, Color.web("#f2660f")),}));
+        colors.widthProperty().bind(scene.widthProperty());
+        colors.heightProperty().bind(scene.heightProperty());
+        root.getChildren().add(colors);
+
+        Group circles = new Group();
+        for (int i = 0; i < 30; i++) {
+            Circle circle = new Circle(150, Color.web("white", 0.05));
+            circle.setStrokeType(StrokeType.OUTSIDE);
+            circle.setStroke(Color.web("white", 0.16));
+            circle.setStrokeWidth(4);
+            circle.setTranslateX(i*18);
+            circle.setTranslateY(i*15);
+            circles.getChildren().add(circle);
+        }
+        circles.setEffect(new BoxBlur(10, 10, 3));
+        root.getChildren().add(circles);
+
+    }
+
+    private void simpleButton(Stage primaryStage)
     {
         Button button = new Button("Klicksi");
 
@@ -24,7 +76,6 @@ public class Main extends Application
 
         primaryStage.setTitle("Gurglar Hälsar");
         primaryStage.setScene(scene);
-        primaryStage.show();
     }
 
     public static void main(String[] args)
